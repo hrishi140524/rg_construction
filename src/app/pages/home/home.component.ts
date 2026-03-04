6e5508388cd5aeba3395a196983bf5fd0b19ff1f
hrishi140524 <hrishimokashi143@gmail.com>
2026-03-04 21:49:19 +0530

    Add About section with styles and content, and integrate it into the Home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../features/hero/hero.component';
+import { AboutComponent } from '../../features/about/about.component';
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
