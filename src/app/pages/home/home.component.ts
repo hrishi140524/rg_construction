978d7c3251e5a8704a4643d022e6d19b08a7e4b5
hrishi140524 <hrishimokashi143@gmail.com>
2026-03-06 21:37:13 +0530

    Add WhatsApp floating button, integrate new services, process, projects, and why choose us sections into Home component, and update styles accordingly

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { AboutComponent } from '../../features/about/about.component';
+import { ServicesComponent } from '../../features/services/services.component';
+import { ProcessComponent } from '../../features/process/process.component';
+import { MaterialComponent } from '../../features/material/material.component'; 
+import { ProjectsComponent } from '../../features/projects/projects.component';
+import { WhyChooseUsComponent } from '../../features/why-choose-us/why-choose-us.component';
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, ServicesComponent, ProcessComponent, MaterialComponent, ProjectsComponent, WhyChooseUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
