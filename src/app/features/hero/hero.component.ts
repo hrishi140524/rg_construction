5b9dc9141621a41f760473aa85c17581051d7aae
hrishi140524 <hrishimokashi143@gmail.com>
2026-03-07 23:58:39 +0530

    Add contract download button, enhance footer styles and layout, and implement smooth scrolling for navigation links

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
