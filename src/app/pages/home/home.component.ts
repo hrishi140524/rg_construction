03fc0203d68a442ab42ccdbfcace23209d400064
hrishi140524 <hrishimokashi143@gmail.com>
2026-03-07 20:58:32 +0530

    Add contact section with styles and functionality, including contact information and Google Maps integration, and update Home component to include the new section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,9 +6,11 @@ import { ProcessComponent } from '../../features/process/process.component';
 import { MaterialComponent } from '../../features/material/material.component'; 
 import { ProjectsComponent } from '../../features/projects/projects.component';
 import { WhyChooseUsComponent } from '../../features/why-choose-us/why-choose-us.component';
+import { ContactComponent } from '../../features/contact/contact.component';
+import { TestimonialsComponent } from '../../features/testimonials/testimonials.component';
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutComponent, ServicesComponent, ProcessComponent, MaterialComponent, ProjectsComponent, WhyChooseUsComponent],
+  imports: [HeroComponent, AboutComponent, ServicesComponent, ProcessComponent, MaterialComponent, ProjectsComponent, WhyChooseUsComponent, ContactComponent, TestimonialsComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
