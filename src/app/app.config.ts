4ef7c3efd0cdb1b19df51e82062bb83f0d5a0b0b
hrishi140524 <hrishimokashi143@gmail.com>
2026-04-14 20:02:41 +0530

    Added Firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,24 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
+// 🔥 Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
+// 🔑 Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAdCyT5lyI9yfMaTiPVxiR2bbe_8c1xk0Q",
+  authDomain: "rgconstruction-cae27.firebaseapp.com",
+  projectId: "rgconstruction-cae27",
+  storageBucket: "rgconstruction-cae27.appspot.com", // ✅ corrected
+  messagingSenderId: "379548768950",
+  appId: "1:379548768950:web:ec1bf07d16a5f4a4f26417",
+  measurementId: "G-RDW5GWJ3GP"
+};
+
+export const appConfig: ApplicationConfig = {
   providers: [
 
     provideZoneChangeDetection({
@@ -17,8 +33,13 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'enabled',
         scrollPositionRestoration: 'enabled'
       })
-    )
+    ),
+
+    // 🔥 ADD THESE LINES (Firebase)
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideAuth(() => getAuth())
 
   ]
-
 };
